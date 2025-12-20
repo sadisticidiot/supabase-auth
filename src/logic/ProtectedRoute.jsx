3b572bd29fe189e6c898aab4d758df074fefcdb6
Clent Sondia <clentsondia124@gmail.com>
2025-12-20 21:42:04 +0800

    Fix loading state for dashboard

--- src/logic/ProtectedRoute.jsx
+++ src/logic/ProtectedRoute.jsx
@@ -2,9 +2,7 @@ import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "./AuthProvider";
 
 export default function ProtectedRoute(){
-    const { session, loading } = useAuth()
-
-    if (loading) return <p>Loading...</p>
+    const { session } = useAuth()
 
     return session ? <Outlet /> : <Navigate to="/login" replace />
 }
